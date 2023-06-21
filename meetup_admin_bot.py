58bb6b78d95d436ae6dd0a0aa668039b2ee50af7
kruser <kruser@yandex.ru>
2023-06-21 14:31:43 +0500

    add create new event

--- meetup_admin_bot.py
+++ meetup_admin_bot.py
@@ -13,7 +13,8 @@ env.read_env()
 
 
 API_TOKEN = env.str('BOT_TOKEN')
-bot = TeleBot(API_TOKEN, state_storage=StateMemoryStorage())
+state_storage = StateMemoryStorage()
+bot = TeleBot(API_TOKEN, state_storage=state_storage)
 
 admin_ids = env.list('ADMIN_IDS', subcast=int)
 
@@ -25,7 +26,8 @@ class NewEventStates(StatesGroup):
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-
+    print(message.chat.id)
+    
     start_keyboard = InlineKeyboardMarkup(
         keyboard=[
             [InlineKeyboardButton('Зарегистрироваться', callback_data='register')],
@@ -37,7 +39,7 @@ def send_welcome(message):
     bot.send_message(
         chat_id=message.chat.id, 
         text=dedent(
-            f"""
+            f'''
             Привествую тебя в Python Meetup!
             
             Тема мероприятия:
@@ -48,7 +50,7 @@ def send_welcome(message):
             * Задавай вопросы прямо в чат-боте.
             * Найди новые контакты.
             
-            """
+            '''
         ),
         reply_markup=start_keyboard   
     )
@@ -77,16 +79,16 @@ def admin_root(call):
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'new_event')
-def admin_create_new_event(call):
-    """Создание нового мероприятия"""
+def admin_request_new_event(call):
+    """запрос создания нового мероприятия"""
     
     chat_id = call.from_user.id
-    bot.set_state(chat_id, NewEventStates.date)
-    bot.send_message(chat_id, 'Введите дату мероприятия в формате ММ.ДД.ГГГГ')
+    bot.set_state(chat_id, NewEventStates.date, chat_id)
+    bot.send_message(chat_id, 'Введите дату мероприятия')
 
 
 @bot.message_handler(state=NewEventStates.date)
-def admin_create_new_event_date(message):
+def admin_request_new_event_date(message):
     """Создание нового мероприятия - Шаг.1 Получение даты"""
     
     bot.send_message(message.chat.id, 'Введите название мероприятия')
@@ -96,15 +98,46 @@ def admin_create_new_event_date(message):
 
 
 @bot.message_handler(state=NewEventStates.name)
-def admin_create_new_event_name(message):
+def admin_request_new_event_name(message):
     """Создание нового мероприятия - Шаг.2 Получение наименования"""
 
+    keyboard = InlineKeyboardMarkup(
+        keyboard=[
+            [
+                InlineKeyboardButton('Создать', callback_data='create_event'),
+                InlineKeyboardButton('Отмена', callback_data='admin')
+            ]
+        ]
+    )
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         data['name'] = message.text
         bot.send_message(
             chat_id=message.chat.id,
-            text=f'Дата: {data["date"]}\n Наименование: {data["name"]}')
+            text=dedent(
+                f'''
+                Подтвердите введенные данные:
+                
+                Название: {data["name"]}
+                Дата: {data["date"]}
+                
+                '''
+            ),
+            reply_markup=keyboard
+        )
     
     bot.delete_state(message.from_user.id, message.chat.id)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'create_event')
+def admin_create_new_event(call):
+    """Создание нового мероприятия"""    
+    chat_id = call.from_user.id
+    with bot.retrieve_data(chat_id, chat_id) as data:
+        # Запись мероприятия в базу данных
+        pass
+
+
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
 
 bot.infinity_polling()
