b3c3607b374c63aae3b4240ab7be508fe0e9c6a9
Michael-Zapivahin <zapivahin@ya.ru>
2023-06-23 08:24:56 +0300

    telegram_id unique

--- meetup/models.py
+++ meetup/models.py
@@ -8,7 +8,7 @@ class Guest(models.Model):
     kind_activity = models.CharField(max_length=200, verbose_name='Вид деятельности', blank=True)
     open_for_contact = models.BooleanField('Открыт для контактов', default=False)
     projects = models.TextField('Проекты', blank=True)
-    telegram_id = models.IntegerField('Телеграм ID')
+    telegram_id = models.IntegerField('Телеграм ID', unique=True)
 
     class Meta:
         verbose_name = 'посетитель'
